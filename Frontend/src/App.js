b3917417ccb83042412581cf6f8c0016644f287b
joynab380-hue <sumayakarim380@gmail.com>
2025-11-13 18:54:31 +0600

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Resources from "./pages/resources";
+
+function App() {
+  return (
+    <div className="App">
+      <Resources />
+    </div>
+  );
+}
+
+export default App;
